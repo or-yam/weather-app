207ddec9eea68cb9e76c7317adef6849009fcea5
Oryam <48219965+or-yam@users.noreply.github.com>
2020-07-22 09:25:43 +0300

    routes added to server and connect to db

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const axios = require('axios');
-const city = require('../model/City');
+const City = require('../model/City');
 
 const router = express.Router();
 
@@ -14,18 +14,39 @@ const getCityWeather = (cityName) => {
 router.get('/weather/:cityName', function (req, res) {
   const { cityName } = req.params;
   getCityWeather(cityName)
-    .then((data) => res.send(data.data))
+    .then((data) => {
+      const kalToCel = 273.15;
+      const city = {
+        name: data.data.name,
+        temperature: Math.floor(data.data.main.feels_like - kalToCel),
+        condition: data.data.weather[0].description,
+        conditionPic: data.data.weather[0].icon,
+      };
+      res.send(city);
+    })
     .catch((err) => res.send(err));
 });
 
+router.get('/cities', function (req, res) {
+  City.find({}).exec((err, data) => res.send(data));
+});
 
-router.get('/cities')
+router.post('/city', function (req, res) {
+  const { name, temperature, condition, conditionPic } = req.body;
+  const city = new City({
+    name,
+    temperature,
+    condition,
+    conditionPic,
+  });
+  city.save().then((s) => res.send(s));
+});
 
-
-// router.post('/city/:cityName')
-
-// router.delete('/city/:cityName')
-
-
+router.delete('/city/:cityName', function (req, res) {
+  const { cityName } = req.params;
+  City.findOneAndDelete({ name: cityName }).exec(
+    res.send('Removed From Favorites')
+  );
+});
 
 module.exports = router;
